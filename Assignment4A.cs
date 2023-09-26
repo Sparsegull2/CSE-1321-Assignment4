5cdc8296931342fe117de6d4ba42f67aaa3e44f8
Sparsegull2 <142937470+Sparsegull2@users.noreply.github.com>
2023-09-26 18:42:03 -0400

    Add files via upload

---
+++ Assignment4A.cs
@@ -0,0 +1,43 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: Assignment4A
+*/
+
+using System;
+
+class Assignment4A
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("[Stopping Time]");
+        float slow = 0, sec = 0,time=0;
+        float dis = 0,mph=0;
+        Console.Write("How fast are you going (miles/hour)? ");
+         mph= int.Parse(Console.ReadLine());
+        Console.Write("How quickly can you decelerate (miles)? ");
+         slow= int.Parse(Console.ReadLine());
+        Console.Write("How often do you want to see updates (in seconds)? ");
+         sec = int.Parse(Console.ReadLine());
+        Console.WriteLine("\nYou are going " +mph+ " MPH when you slam on the brakes!");
+        
+        while (mph > 0)
+        {
+                       
+            dis += mph*5280/3600;
+            mph -= slow ;
+            time += 1;
+            if (mph < 0) break;
+            if (time % sec == 0)
+            {
+                if (time == 1) Console.WriteLine("At " + time + " second, you have traveled " + dis.ToString("n4") + " feet and are now moving at " + mph + " MPH.");
+                else Console.WriteLine("At " + time + " seconds, you have traveled " + dis.ToString("n4") + " feet and are now moving at " + mph + " MPH.");
+            }
+        }
+        Console.WriteLine("\nYou took "+time+" seconds to stop and traveled "+dis.ToString("n4") + " feet during that time!");
+    }
+}
---
+++ Assignment4B.cs
@@ -0,0 +1,50 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: Assignment4B
+*/
+
+using System;
+
+class Assignment4B
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("[Plus Sign ASCII Art]");
+        Console.Write("Font-size: ");
+        int dim= int.Parse(Console.ReadLine());
+        Console.Write("Font-weight: ");
+        int weight= int.Parse(Console.ReadLine());
+        Console.WriteLine("Drawing with text...");
+        int size = (dim * 2) + weight;
+
+        int midA = dim + 1;
+        int midB = dim + weight;
+        
+        
+        for (int i = 1; i <= size; i++)
+        {
+            if (i>=midA&&i<=midB)
+            {
+                for (int k = 1; k< size;k++) {
+                    Console.Write("-");
+                }
+            }
+            else
+            {
+                for (int j = 1; j<=size; j++)
+                {
+                    if (size / 2 == j) { 
+                    Console.Write("|");
+                    }
+                    Console.WriteLine(" ");
+                }
+
+            }
+        }
+    }
+}
